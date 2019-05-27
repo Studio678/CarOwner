233bf7e1553346e7eef6c750f910b124c660df60
studio678 <murzinaa1987@gmail.com>
2019-05-27 22:52:28 +0500

    theme 5 lesson 5 programm 1 rev.a1 tag th5l5p1ra1

--- src/com/studio678/Main.java
+++ src/com/studio678/Main.java
@@ -1,9 +1,9 @@
-/**
- *  Написать программу, которая будет выдавать имя владельца автомобиля по его номеру.
- *  Программа должна быть умной: если у неё в памяти номера нет,
- *  она должна попросить ввести его имя и запомнить.
- *
- *  author studio678 24.05.2019
+/*
+   Написать программу, которая будет выдавать имя владельца автомобиля по его номеру.
+   Программа должна быть умной: если у неё в памяти номера нет,
+   она должна попросить ввести его имя и запомнить.
+
+   author studio678 24.05.2019
  */
 
 package com.studio678;
@@ -11,44 +11,90 @@ package com.studio678;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    private static String readBlock(boolean key) throws IOException {
-        if (key) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Input 'LIST' for print all cars in List");
-            System.out.println("Input number of car or command: ");
-            String str = reader.readLine().trim();
-            System.out.println();
-            return str;
-        } else{
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Input name of owner: ");
-            String str = reader.readLine().trim();
-            System.out.println();
-            return str;
-        }
 
+    private static String readBlock(String message) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(message);
+        String str = reader.readLine().trim();
+        System.out.println();
+        return str;
     }
 
+    private static void checkCarBase(TreeMap<String, String> carBase, String command) throws IOException {
+        //check what to do with number
+        if (carBase.containsKey(command)) {//print if in base
+            System.out.println("Owner of car with number " + command + " is: " + carBase.get(command));
+            System.out.println();
+        } else {//please input owner name and put record to base
+            String message = "input car owner name: ";
+            String thisCarOwner = readBlock(message);
+            carBase.put(command, thisCarOwner);
+        }
+    }
+
+    private static String getKeyByValue(TreeMap<String,String> map, String value){
+
+        for(Map.Entry<String,String> thisKey: map.entrySet()){
+            if(value.equals(thisKey.getValue())){
+                return thisKey.getKey();
+            }
+        }
+        return null;
+    }
+
+
+    private final static Pattern CAR_NUMBER_PATTERN = Pattern.compile("([А-Я][0-9]{3}[А-Я]{2}(?:[0-9]+)?)");
+
     public static void main(String[] args) throws IOException {
-        // write your code here
         TreeMap<String, String> carBase = new TreeMap<>();
         while (true){
-            String command = readBlock(true);
-            if (command.equals("LIST")) {
+            System.out.println("Input 'LIST' for print all cars in List");
+            String message = "input car number, name of owner or LIST";
+            String command = readBlock(message);
+
+            if (command.equals("LIST")) {//print all cars in base
                 for (String carNumber : carBase.keySet()) {
                     System.out.println(carNumber + " => " + carBase.get(carNumber));
                 }
                 break;
-            } else if (carBase.containsKey(command)) {
-                System.out.println("Owner of car with number " + command + " is: " + carBase.get(command));
-                System.out.println();
-            } else {
-                String thisCarOwner = readBlock(false);
-                carBase.put(command, thisCarOwner);
+            } else{//check input
+                Matcher matcher = CAR_NUMBER_PATTERN.matcher(command);
+                if(matcher.matches()){//if number do
+                    checkCarBase(carBase, command);//use car checker
+                }else if(carBase.containsValue(command)) {//user input name and its in base
+                    String carNumber = getKeyByValue(carBase, command);//
+                    if(carNumber == null){//error checker
+                        System.out.println("Something gone wrong");
+                    }else {//print name of owner and car number if they contains in base
+                        System.out.println("Owner of car with number " + carNumber + " is: " + command);
+                        System.out.println();
+                    }
+                }else {//name not in base, please input car number and add to base record
+                    String message2 = "Input car number:";
+                    String thisCarNumber = readBlock(message2);
+                    Matcher thisMatcher = CAR_NUMBER_PATTERN.matcher(thisCarNumber);
+                    if(thisMatcher.matches()){//check if input correct
+                        carBase.put(thisCarNumber, command);//if so put record
+                    }else {//other way please input again
+                        while (true) {
+                            String thisCarNumber1 = readBlock(message2);
+                            thisMatcher = CAR_NUMBER_PATTERN.matcher(thisCarNumber1);
+                            if(thisMatcher.matches()){
+                                carBase.put(thisCarNumber1, command);
+                                break;
+                            }
+                        }
+                    }
+                }
             }
+
+
         }
     }
 }
